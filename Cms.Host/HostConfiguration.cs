c71ad3e56e687eb0f5b414981e521e9576f180ac
wildRaccoon <chukvm@gmail.com>
2021-04-08 14:47:29 +0300

    add host config

---
+++ Cms.Host/HostConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace Cms.Host
+{
+    public class HostConfiguration
+    {
+        public string ClusterId { get; set; }
+        public string ServiceId { get; set; }
+        public bool CreateShardKey { get; set; } = false;
+        public bool UseLocalCluster { get; set; }
+        public MongoDbHostConfig MongoDb { get; set; }
+    }
+
+    public class MongoDbHostConfig
+    {
+        public string ClusteringDatabase { get; set; }
+        public string Database { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
--- Cms.Host/Program.cs
+++ Cms.Host/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Cms.Orls;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
 
 namespace Cms.Host
 {
@@ -13,49 +15,75 @@ namespace Cms.Host
     {
         static async Task Main(string[] args)
         {
-            var connectionString = "mongodb://admin:pass12345@localhost";
-            var createShardKey = false;
-            var dbName = "CmsAuth";
+            var hostConfig = BuildConfig(args);
 
             var host = new HostBuilder()
                 .UseOrleans(siloBuilder =>
                 {
-                    siloBuilder                        
-                        .UseDashboard(conf => {
+                    siloBuilder
+                        .UseDashboard(conf =>
+                        {
                             conf.Port = 8080;
                             conf.HostSelf = true;
                         })
-                        .ConfigureCms(dbName)
+                        .ConfigureCms(hostConfig.MongoDb.Database)
                         .Configure<ClusterOptions>(opt =>
                         {
-                            opt.ClusterId = "debug";
-                            opt.ServiceId = "cms_app";
+                            opt.ClusterId = hostConfig.ClusterId;
+                            opt.ServiceId = hostConfig.ServiceId;
                         })
                         .ConfigureEndpoints(siloPort: 11111, gatewayPort: 30000)
-                        .UseMongoDBClient(connectionString)
+                        .UseMongoDBClient(hostConfig.MongoDb.ConnectionString)
                         .AddMongoDBGrainStorage("Cms", config =>
                         {
-                            config.DatabaseName = dbName;
-                        })
-                        .UseLocalhostClustering();
-                        //.UseMongoDBClustering(config => {
-                        //    config.DatabaseName = dbName;
-                        //    config.CreateShardKeyForCosmos = createShardKey;
-                        //})
-                        //.UseMongoDBReminders(config => {
-                        //    config.CreateShardKeyForCosmos = createShardKey;
-                        //    config.DatabaseName = dbName;
-                        //});
+                            config.DatabaseName = hostConfig.MongoDb.Database;
+                        });
+
+                    if (hostConfig.UseLocalCluster)
+                    {
+                        siloBuilder.UseLocalhostClustering();
+                    }
+                    else
+                    {
+                        siloBuilder
+                            .UseMongoDBClustering(config =>
+                            {
+                                config.DatabaseName = hostConfig.MongoDb.ClusteringDatabase;
+                                config.CreateShardKeyForCosmos = hostConfig.CreateShardKey;
+                            })
+                            .UseMongoDBReminders(config =>
+                            {
+                                config.CreateShardKeyForCosmos = hostConfig.CreateShardKey;
+                                config.DatabaseName = hostConfig.MongoDb.ClusteringDatabase;
+                            });
+                    }
                 })
-                .ConfigureLogging(logBuilder => {
+                .ConfigureLogging(logBuilder =>
+                {
                     logBuilder.AddConsole();
                 })
-                .ConfigureWebHostDefaults(webBuilder => {
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
                     webBuilder.UseStartup<Startup>();
                 })
                 .Build();
 
             await host.RunAsync();
+        }
+
+        private static HostConfiguration BuildConfig(string[] args)
+        {
+            var envName = args.FirstOrDefault() ?? "Dev";
+
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{envName}.json", optional:true)
+                .Build();
+
+            var hostConfig = new HostConfiguration();
+            config.GetSection("CmsHost").Bind(hostConfig);
+
+            return hostConfig;
         }
     }
 }
